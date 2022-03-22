1c2457c500b2d1f4f2941129d6999ab695197ded
AlHood <alastair.hood85@gmail.com>
2022-03-22 15:30:14 +0000

    Better courses map
    Blessings upon you, all of your days

--- WASapp/views.py
+++ WASapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Please go to /courses/ !")
+    return HttpResponse("Please go to  <a href='/courses/'>COURSES</a> !")
 # Create your views here.
 
 def courses(request):
